7384706871179c70e3e97be86d40ea10bf8a203e
Rob Watson <rob@mixlr.com>
2017-07-14 13:06:07 +0100

    Remove stray require

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ require 'bundler/setup'
 require 'rack/filter_param'
 require 'rack/test'
 require 'byebug'
-require 'ap'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
